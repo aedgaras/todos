d54c9b4d1f2c73dada0f1ee63badc1bd4f23b4cd
Edgaras Adomavičius <edgarasgreek@icloud.com>
2023-07-21 17:51:57 +0300

    Added supabase and it's session provider.

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,12 +1,14 @@
 import { CssVarsProvider } from '@mui/joy';
 import React, { PropsWithChildren, Suspense } from 'react';
 import { LoadingSpinner } from '../components';
+import { SessionProvider } from './SessionProvider';
 
 export const AppProvider: React.FC<PropsWithChildren> = (props) => {
-
     return (
         <CssVarsProvider>
-            <Suspense fallback={<LoadingSpinner />}>{props.children}</Suspense>
+            <Suspense fallback={<LoadingSpinner />}>
+                <SessionProvider>{props.children}</SessionProvider>
+            </Suspense>
         </CssVarsProvider>
     );
 };
