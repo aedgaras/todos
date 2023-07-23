0159f78fe68013d0a8ec67109bb64c70ba612cdf
Edgaras Adomavičius <edgarasgreek@icloud.com>
2023-07-23 22:25:41 +0300

    Install query, state manager

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,17 +1,22 @@
 import { CssVarsProvider } from '@mui/joy';
 import React, { Suspense } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 import { LoadingSpinner } from '../components';
 import { router } from '../routes';
 import { SessionProvider } from './SessionProvider';
 
 export const AppProvider: React.FC = () => {
+    const queryClient = new QueryClient();
+
     return (
         <CssVarsProvider>
             <Suspense fallback={<LoadingSpinner />}>
-                <SessionProvider>
-                    <RouterProvider router={router} />
-                </SessionProvider>
+                <QueryClientProvider client={queryClient}>
+                    <SessionProvider>
+                        <RouterProvider router={router} />
+                    </SessionProvider>
+                </QueryClientProvider>
             </Suspense>
         </CssVarsProvider>
     );
